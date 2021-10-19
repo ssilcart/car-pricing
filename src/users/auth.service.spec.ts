ef689b31b5324a8dd7bfed346eaad1668d36be7a
Santiago Silva Cartagena <santiago.silcart@gmail.com>
2021-10-18 22:35:01 -0500

    test(auth): signin and sign up tests implementation

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
@@ -5,9 +6,10 @@ import { UsersService } from './users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({
@@ -29,7 +31,44 @@ describe('AuthService', () => {
     service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('signup', () => {
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+    });
+
+    it('should return a hased password', async () => {
+      const signUpUser = {
+        email: 'foo@bar.com',
+        password: 'abc123',
+      };
+      const user = await service.signup(signUpUser.email, signUpUser.password);
+      expect(user.password).not.toEqual(signUpUser.password);
+      const [password, hash] = user.password.split('.');
+      expect(password).toBeDefined();
+      expect(hash).toBeDefined();
+    });
+
+    it('should fail if email already in use', async () => {
+      const signUpUser = {
+        email: 'foo@bar.com',
+        password: 'abc123',
+      };
+      fakeUsersService.find = () => Promise.resolve([signUpUser as User]);
+      await expect(
+        service.signup(signUpUser.email, signUpUser.password),
+      ).rejects.toThrow(BadRequestException);
+    });
+  });
+
+  describe('signin', () => {
+    it('should fail if not registered Email', async () => {
+      const signUpUser = {
+        email: 'foo@bar.com',
+        password: 'abc123',
+      };
+      await expect(
+        service.signin(signUpUser.email, signUpUser.password),
+      ).rejects.toThrow(NotFoundException);
+    });
   });
 });
