65f79a74a43c7db98d66ed01c84bc52155138cf5
Santiago Silva Cartagena <santiago.silcart@gmail.com>
2021-10-18 20:59:46 -0500

    test(auth): implemented basic tests on auth service

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -4,6 +4,6 @@ export class AuthGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
 
-    return request;
+    return request.session.userId;
   }
 }
